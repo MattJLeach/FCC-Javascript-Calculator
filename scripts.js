699df96909576c653086bd0fd6f31978b2ad47b0
Matt Leach <email@mattleach.info>
2016-10-20 21:02:26 +0100

    working on JS

--- scripts.js
+++ scripts.js
@@ -3,7 +3,12 @@ $(function() {
 	// declare all variables
 	var result = 0;
 	var entry = '';
+	var decimal = false;
+	var type = '';
+	var numbers = [0,1,2,3,4,5,6,7,8,9];
+	var operators = ['/','*','-','+'];
 
+	// Function to update the display
 	function updateOutput(value) {
 		if(value == 'entry') {
 			$('#output').text(entry);
@@ -12,9 +17,42 @@ $(function() {
 		}
 	}
 
+	// Function to determine the type of entry
+	function determineType(value) {
+		for(var i = 0; i < numbers.length; i++) {
+			if(value == numbers[i]) {
+				type = 'number';
+			}
+		}
+
+		for(var i = 0; i < operators.length; i++) {
+			if(value == operators[i]) {
+				type = 'operator';
+			}
+		}
+
+		if(value == 'clr') {
+			type = 'clear';
+		}
+
+		if(value == '.') {
+			type = 'decimal';
+		}
+
+		if(value == '=') {
+			type = 'equals';
+		}
+	}
+
 	$('button').click(function() {
 		entry = $(this).attr('value');
-		console.log(entry);
+		determineType(entry);
+		
+
+
+
+
+		console.log(type);
 		updateOutput('entry');
 	});
 
